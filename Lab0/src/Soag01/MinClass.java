59134924ce48f84d57aaa42ab130e1fc95e7fe66
Sofia.Ågren <sofiaagren1@hotmail.com>
2019-09-30 15:56:53 +0200

    Först lagringen

---
+++ Lab0/src/Soag01/MinClass.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package Soag01;
+
+/**
+ * @author 19soag01 (Sofia Ågren)
+ *
+ */
+public class MinClass {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+	}
+
+}
