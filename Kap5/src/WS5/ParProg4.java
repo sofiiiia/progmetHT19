e3ec822ea0b5a4fc57bc0a8d18b216cd9a9a64b0
sofiiiia <55987397+sofiiiia@users.noreply.github.com>
2019-10-03 18:39:41 +0200

    Update ParProg4.java

--- Kap5/src/WS5/ParProg4.java
+++ Kap5/src/WS5/ParProg4.java
@@ -1,17 +1,19 @@
+/**
+ * 
+ */
+package sofia;
 
-		package WS5;
+import java.util.Scanner;
 
-		import java.util.Scanner;
+/**
+ * @author Sofia
+ *
+ */
+public class ParProg_4 {
 
-		/**
-		 * @author Sofia Ågren och Anel Duisemaliyeva
-		 *
-		 */
-		public class ParProg4 {
-
-			/**
-			 * @param args
-			 */
+	/**
+	 * @param args
+	 */
 	public static Scanner in = new Scanner(System.in);
 	static double summa =0.0;
 	static double största = 0.0;
@@ -21,10 +23,12 @@
 		double summa = a + b + c;
 		double största = Math.max(Math.max(a, b), c);
 		double minsta = Math.min(Math.min(a, b), c);
-		boolean första = true;
-		System.out.print("Mata in ett tal: ");
-		System.out.print("Mata in ett tal (eller Stop eller Avsluta): ");
+		return 0.0;
+		
+	}	
+	public static boolean avsluta(String str) {
 		String s = in.next();
+		boolean första = true;
 		boolean avsluta = false;
 		while (!avsluta) {
 			if (!avsluta) {
@@ -36,23 +40,23 @@
 					största = tal;
 				första = false;
 			}
-		}
-		
-		if ("Stop".equals(s.trim()) || "Avsluta".equals(s.trim())) {
+		}if ("Stop".equals(s.trim()) || "Avsluta".equals(s.trim())) {
 			System.out.println("Avslutar");
 			boolean avslutar = true;
-		} else {
-		System.out.println("Fel, mata in ett nytt tal");
-		System.out.print("Mata in ett tal: ");
+			} else {
+				System.out.println("Fel, mata in ett nytt tal");
+				System.out.print("Mata in ett tal: ");
 		}
 		while ( ! avsluta && ! in.hasNextDouble()) {
 		}
-		return 0.0;
-
+		return avsluta;
+	}
 	public static void main(String[] args) {
 		System.out.println("Mata in flyttal tills du skriver in Stop eller Avsluta");
 		System.out.println("För varje tal skriver du ut den accumulativa summan");
 		System.out.println("samt det så långt minsta och största talet");
+		System.out.print("Mata in ett tal: ");
+		System.out.print("Mata in ett tal (eller Stop eller Avsluta): ");
 		System.out.printf("Summa %f, Min %f Max %f%n", summa, minsta, största);
 		System.out.printf("Summa %f, Min %f Max %f%n",summa,minsta,största);
 		System.out.println("Tack för att du använt programmet");
@@ -60,5 +64,4 @@
 		in.close();
 		System.exit(0);
 		}
-	}
-}
+	} 
