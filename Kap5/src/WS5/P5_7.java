84b4f0b5e786a8fc5196c55061c6616b92b8ba3d
Sofia.Ågren <sofiaagren1@hotmail.com>
2019-10-04 12:52:28 +0200

    # WARNING: head commit changed in the meantime
    test

--- Kap5/src/WS5/P5_7.java
+++ Kap5/src/WS5/P5_7.java
@@ -17,7 +17,7 @@ public class P5_7 {
 		str = str.trim();
 		int countWords = 1;
 		for(int i = 0; i < str.length(); i++) {
-			if(str.charAt(i) == ' ') {
+			if(str.charAt(i) == ' ' && str.charAt(i+1) != ' ') {
 				countWords++;
 			}	
 		}
